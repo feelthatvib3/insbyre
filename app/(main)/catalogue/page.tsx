6c7e1868518b3e668ee62d76b6275c56e6ad3228
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2025-06-24 17:13:12 +0200

    feat: handle category query parameter

--- app/(main)/catalogue/page.tsx
+++ app/(main)/catalogue/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 
-import { useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 import { type CatalogueFilters, CatalogueFiltersSidebar } from 'features/catalogue-filters';
 
 import { ProductList } from 'entities/product';
 
 export default function Page() {
-  const [filters, setFilters] = useState<CatalogueFilters>({});
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const categoryFromUrl = searchParams?.get('c') ?? undefined;
+
+  const [filters, setFilters] = useState<CatalogueFilters>({ category: categoryFromUrl });
+
+  useEffect(() => {
+    setFilters((prev) => ({ ...prev, category: categoryFromUrl }));
+  }, [categoryFromUrl]);
+
+  const handleFiltersChange = (newFilters: CatalogueFilters) => {
+    setFilters(newFilters);
+
+    const params = new URLSearchParams();
+    if (newFilters.category) params.set('c', newFilters.category);
+    else params.delete('c');
+
+    router.push(`?${params.toString()}`, { scroll: false });
+  };
 
   return (
     <div>
@@ -15,7 +34,7 @@ export default function Page() {
         <h1 className="font-display text-4xl uppercase md:text-5xl">Каталог</h1>
         <div className="flex gap-x-8">
           <div className="hidden shrink-0 md:block">
-            <CatalogueFiltersSidebar filters={filters} onFiltersChange={setFilters} />
+            <CatalogueFiltersSidebar filters={filters} onFiltersChange={handleFiltersChange} />
           </div>
           <ProductList filters={filters} />
         </div>
