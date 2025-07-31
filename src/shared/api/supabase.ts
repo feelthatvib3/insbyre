385b1cc38a1bcbd6c80f06b459300d88b0bc510c
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2025-07-31 11:51:37 +0200

    fix: move publishable key to the code

--- src/shared/api/supabase.ts
+++ src/shared/api/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://txagwhibhgydyfzwfyol.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = 'sb_publishable_GlAe4HN7bSkAX4jniFCDMw_6f4Nf3VY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
