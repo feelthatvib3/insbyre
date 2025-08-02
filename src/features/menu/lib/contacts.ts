40510b571e6d2f6c0838ce50c859acdf5d035e70
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2025-08-02 14:36:24 +0200

    feat: modify contacts

--- src/features/menu/lib/contacts.ts
+++ src/features/menu/lib/contacts.ts
@@ -3,6 +3,6 @@ import { InstagramLogoIcon, TelegramLogoIcon } from '@phosphor-icons/react';
 import type { MenuItem } from 'features/menu';
 
 export const contacts: MenuItem[] = [
-  { name: 'Telegram', href: 'https://t.me/feelthatvib3', icon: TelegramLogoIcon },
-  { name: 'Instagram', href: 'https://instagram.com/feelthatvib3', icon: InstagramLogoIcon }
+  { name: 'Telegram', href: 'https://t.me/insbyre', icon: TelegramLogoIcon },
+  { name: 'Instagram', href: 'https://www.instagram.com/insbyre.brand', icon: InstagramLogoIcon }
 ];
--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
     <footer className="border-t-united-nations-blue/10 border-t">
-      <div className="mx-auto flex max-w-[1200px] items-center justify-between p-4 text-xs xl:px-0">
+      <div className="mx-auto flex max-w-[1200px] items-center justify-between gap-x-4 p-4 text-xs xl:px-0">
         <p className="text-muted-foreground">Товары предоставляет самозанятый — ИНН 520300699820</p>
-        <p className="text-muted-foreground">{new Date().getFullYear()}</p>
+        <p className="text-muted-foreground">&copy; {new Date().getFullYear()} INSBYRE</p>
       </div>
     </footer>
   );
